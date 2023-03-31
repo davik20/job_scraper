8d803431a74da4ae7834556d18ff5553e01a586e
davik20 <onyejivic@gmail.com>
2023-03-31 14:46:10 +0100

    update

---
+++ api/run_scrape.ts
@@ -0,0 +1,76 @@
+const fetch = require('node-fetch')
+import * as cheerio from 'cheerio';
+import { companies, Company } from '../src/constants/companies';
+import { createClient } from '@supabase/supabase-js';
+import puppeteer from 'puppeteer'
+import Job from '../src/interfaces/jobs';
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+
+
+
+
+
+
+// Replace these with your Supabase API details
+
+const supabaseUrl = "https://dkwqunprrqmhmbtnfuzs.supabase.co";
+const supabaseKey:any = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function storeJobs(jobs: Job[]): Promise<void> {
+    for (const job of jobs) {
+      const { title, company } = job;
+  
+      const { error } = await supabase
+        .from('jobs')
+        .insert([{ title, company }], { });
+  
+        if (error && error.code === '23505') {
+          console.log('Duplicate entry:', job);
+        } else if(error){
+          console.error('Error inserting job:', error);
+        }
+    }
+  }
+  
+  const scrape = async (companies: Company[]): Promise<Job[][]> => {
+    const browser = await puppeteer.launch();
+    const promises = companies.map(async (company: Company) => {
+      const jobs: Job[] = [];
+      const page = await browser.newPage();
+      await page.goto(company.url);
+      const content = await page.content();
+      const $ = cheerio.load(content);
+      const $jobs = $(company.selector);
+  
+      $jobs.each((_, e) => {
+        const title = $(e).text().trim();
+        jobs.push({ title, company: company.name });
+      });
+  
+      return jobs;
+    });
+  
+    const results = await Promise.all(promises);
+    await browser.close();
+    return results;
+  };
+
+
+  
+
+  
+
+  
+  const main = async () => {
+    const jobArrays = await scrape(companies);
+    const jobs = jobArrays.flat();
+    await storeJobs(jobs);
+    console.log(jobs);
+  };
+  
+  main();
