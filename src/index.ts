610767160fef00e25cf5eb6b703fc33f9924da48
davik20 <onyejivic@gmail.com>
2023-03-31 16:18:32 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // app.ts
 
-
 import { run_scrape } from '../api/run_scrape'
 
 import express from 'express';
@@ -10,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api/run_scrape', run_scrape);
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
